dd75840c0c8037763a7d6d7c552100bf3a9ecd75
zakaria <himranezakaria2@gmail.com>
2025-09-10 12:30:14 +0200

    Updated findDuplicates

--- src/commands/findDuplicates.ts
+++ src/commands/findDuplicates.ts
@@ -1,12 +1,26 @@
 import chalk from 'chalk'
-import fs from 'fs'
+import fs, { readFileSync } from 'fs'
 import path from 'path'
+import crypto, { createHash } from 'crypto'
+
+const getFileHash = (filePath: string): string => {
+  //read file contents
+  const fileBuffer = readFileSync(filePath)
+
+  //create a hash object
+  const hash = createHash('sha256')
+  
+  //pass the file buffer to the hash object
+  hash.update(fileBuffer)
+
+  return hash.digest('hex')
+}
 
 export const findDuplicate = (targetPath: string): void => {
   const absPath = path.resolve(targetPath)
 
   if(!fs.existsSync(absPath)){
-    console.log(chalk.red("Incorrect path / folder doesn't exists"))
+    console.log(chalk.red("Incorrect path / folder not found"))
     return 
   }
 
@@ -17,5 +31,29 @@ export const findDuplicate = (targetPath: string): void => {
     return
   }
 
+  const hashMap = new Map<string, string[]>()
+  for(const file of files){
+    const filePath = path.join(absPath, file)
+    const stat: fs.Stats = fs.statSync(filePath)
+
+    if(stat.isFile()){
+      const hash = getFileHash(filePath)
+
+      if(!hashMap.has(hash)){
+        hashMap.set(hash, [])
+      }
+
+      hashMap.get(hash)?.push(filePath)
+    }
+  }
+
+  for(const [hash, hashedFiles] of hashMap.entries()){
+    if (hashedFiles.length > 1) {
+      console.log(`${chalk.bgWhite('Duplicated files:')} \n`)
+      for(const f of hashedFiles){
+        console.log(' -', f)
+      }
+    }
+  }
   
 }
